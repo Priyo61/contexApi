76f69636a2bb21c49a397cdefbf7b6f9266fb650
Priyobrata Sorokhaibam <priyobratasorokhaibam@Priyobratas-MacBook-Air.local>
2025-10-23 03:28:09 +0530

    use of contect api

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,7 +1,45 @@
-import "./App.css";
+import { createContext, useContext, useState } from "react";
+// step:1 defined createcontext
+const BulbContext = createContext();
 
 function App() {
-  return <div>hi</div>;
+  const [bulbOn, setBulbOn] = useState(true);
+  return (
+    <div>
+      <h1>Here am learning context api</h1>
+      <div>
+        <BulbContext.Provider value={{ bulbOn, setBulbOn, name: "soro" }}>
+          <Light></Light>
+        </BulbContext.Provider>
+      </div>
+    </div>
+  );
+}
+
+function Light() {
+  const { name } = useContext(BulbContext);
+  return (
+    <div>
+      {name}
+      <BulbOn></BulbOn>
+      <ToggleBulb></ToggleBulb>
+    </div>
+  );
+}
+function BulbOn() {
+  const { bulbOn } = useContext(BulbContext);
+  return <div>{bulbOn ? "Bulb ON" : "Bulb OFF"}</div>;
+}
+function ToggleBulb() {
+  const { bulbOn, setBulbOn } = useContext(BulbContext);
+  function switchOnOff() {
+    setBulbOn(!bulbOn);
+  }
+  return (
+    <div>
+      <button onClick={switchOnOff}>Toggle</button>
+    </div>
+  );
 }
 
 export default App;
