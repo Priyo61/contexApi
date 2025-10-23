2f5a2e9a146c41a3fafcb1dd54591641f52d96e3
Priyobrata Sorokhaibam <priyobratasorokhaibam@Priyobratas-MacBook-Air.local>
2025-10-24 02:46:27 +0530

    add recoil

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,45 +1,7 @@
-import { createContext, useContext, useState } from "react";
-// step:1 defined createcontext
-const BulbContext = createContext();
+//
 
-function App() {
-  const [bulbOn, setBulbOn] = useState(true);
-  return (
-    <div>
-      <h1>Here am learning context api</h1>
-      <div>
-        <BulbContext.Provider value={{ bulbOn, setBulbOn, name: "soro" }}>
-          <Light></Light>
-        </BulbContext.Provider>
-      </div>
-    </div>
-  );
-}
+// Recoil again
 
-function Light() {
-  const { name } = useContext(BulbContext);
-  return (
-    <div>
-      {name}
-      <BulbOn></BulbOn>
-      <ToggleBulb></ToggleBulb>
-    </div>
-  );
+export default function App() {
+  return <div>hi</div>;
 }
-function BulbOn() {
-  const { bulbOn } = useContext(BulbContext);
-  return <div>{bulbOn ? "Bulb ON" : "Bulb OFF"}</div>;
-}
-function ToggleBulb() {
-  const { bulbOn, setBulbOn } = useContext(BulbContext);
-  function switchOnOff() {
-    setBulbOn(!bulbOn);
-  }
-  return (
-    <div>
-      <button onClick={switchOnOff}>Toggle</button>
-    </div>
-  );
-}
-
-export default App;
