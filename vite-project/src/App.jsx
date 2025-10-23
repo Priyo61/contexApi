1147db24f7d0cfd65e99748ff557aa85060ed422
Priyobrata Sorokhaibam <priyobratasorokhaibam@Priyobratas-MacBook-Air.local>
2025-10-24 04:05:12 +0530

    add recoil use only react@18

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,7 +1,49 @@
-//
-
 // Recoil again
-
-export default function App() {
-  return <div>hi</div>;
+import { RecoilRoot, useRecoilValue, useSetRecoilState } from "recoil";
+import { counterAtom } from "./store/atoms/counter";
+function App() {
+  return (
+    <RecoilRoot>
+      <Counter></Counter>
+    </RecoilRoot>
+  );
 }
+
+function Counter() {
+  return (
+    <div>
+      <CurrentCounter></CurrentCounter>
+      <Increase></Increase>
+      <Decrease></Decrease>
+    </div>
+  );
+}
+
+function CurrentCounter() {
+  const count = useRecoilValue(counterAtom);
+  return <div>{count}</div>;
+}
+function Increase() {
+  const setCounter = useSetRecoilState(counterAtom);
+  function increase() {
+    setCounter((c) => c + 1);
+  }
+  return (
+    <div>
+      <button onClick={increase}>Increase</button>
+    </div>
+  );
+}
+function Decrease() {
+  const setCounter = useSetRecoilState(counterAtom);
+  function decrease() {
+    setCounter((c) => c - 1);
+  }
+  return (
+    <div>
+      <button onClick={decrease}>decrease</button>
+    </div>
+  );
+}
+
+export default App;
